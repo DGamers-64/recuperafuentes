c61aa492a7dd53be6505a935c9d3daf1a77208a7
Daniel Fuentes Vila <daniel.fuentes.vila@gmail.com>
2026-03-06 08:57:34 +0100

    Router hecho y componente de IniCio inicializado

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
+    <router-view></router-view>
   </p>
 </template>
 
---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import IniCio from "@/components/IniCio.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      component: IniCio
+    }//,
+    // {
+    //   path: "/empleados",
+    //   component: EmpLeados
+    // },
+    // {
+    //   path: "/tareas",
+    //   component: TaReas
+    // },
+    // {
+    //   path: "/estadisticas",
+    //   component: EstaDisticas
+    // },
+    // {
+    //   path: "/about",
+    //   component: AcerCa
+    // }
+  ],
+})
+
+export default router
